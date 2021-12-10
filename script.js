a1b8c0c47df5ac2049a3335a89ab983657a5f170
oansky <oana.anghelachi@gmail.com>
2021-12-10 17:06:56 +0100

    Add 'change voice' functionality

--- script.js
+++ script.js
@@ -117,6 +117,12 @@ function speakText() {
 	speechSynthesis.speak(message)
 }
 
+// Set voice
+function setVoice(e) {
+	message.voice = voices.find(voice => voice.name === e.target.value)
+	// the VOICE PROPERTY of the MESSAGE OBJ; the VOICES ARRAY
+}
+
 // Voices changed
 speechSynthesis.addEventListener("voiceschanged", getVoices)
 
@@ -129,6 +135,9 @@ toggleBtn.addEventListener("click", () =>
 closeBtn.addEventListener("click", () =>
 	document.getElementById("text-box").classList.remove("show")
 )
+
+// Change voice
+voicesSelect.addEventListener("change", setVoice)
 
 getVoices()
 
